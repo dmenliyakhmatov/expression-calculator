f8b68e1719c96725b3016829580c25958a8ae3e8
dmenliyakhmatov <51416462+dmenliyakhmatov@users.noreply.github.com>
2020-03-19 21:50:25 +0300

    feat: start task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,47 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    console.log(Boolean('('));
+    let exprArr;
+    let bracketsCount = 0;
+    if (expr.includes(' ')) {
+        exprArr = expr.split(' ');
+    } else {
+        exprArr = expr.split('');
+    }
+
+    exprArr.forEach(element => {
+        if (element ==='(') {
+            bracketsCount++;
+        }
+        if (element === ')') {
+            bracketsCount--;
+        }
+    });
+    
+    if(bracketsCount != 0) { 
+        throw "ExpressionError: Brackets must be paired";
+    }
+
+    exprArr.forEach(element => {
+        if (element ==='/' && exprArr[element+1] === '0') {
+            throw "TypeError: Division by zero."
+        }
+    });
+
+    let stackNumbers = [];
+    let stackSign = [];
+    let priority = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2
+    }
+    exprArr.forEach( element => {
+        if ( Number(element) === NaN){
+            stackSign.push
+         }
+    })
 }
 
 module.exports = {
